21c6372cce50995716efbe3d249fb3f6f63f7907
yamoo9 <yamoo9@naver.com>
2020-02-07 20:07:32 +0900

    서비스 워커 활성화

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import * as serviceWorker from './utils/serviceWorker';
+import * as serviceWorker from './utils/serviceWorker';
+
 import App from './App/App';
 import './styles/index.sass';
 import './utils/checkIE';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
-// serviceWorker.register();
+serviceWorker.register();
